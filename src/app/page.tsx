ff9f9240e2edfeae6bd8122025a2543639c8c1b3
anurag <anuragbadwahe@gmail.com>
2024-03-10 00:26:38 +0530

    Created Courses card item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Courses from "@/components/Courses";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
@@ -7,6 +8,7 @@ export default function Home() {
     <>
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
+    <Courses/>
    </main>
     </>
   );
