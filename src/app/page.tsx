d1a584a86ad0ee3aa302718805fa1a68d77c5ec5
anurag <anuragbadwahe@gmail.com>
2024-03-11 20:05:48 +0530

    Created WhyChoose section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Courses from "@/components/Courses";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
     <Courses/>
+    <WhyChooseUs/>
    </main>
     </>
   );
