a508a1d7f72234787f3e22b13f394627dd584813
anurag <anuragbadwahe@gmail.com>
2024-03-15 23:39:00 +0530

    Made some changes in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Courses from "@/components/Courses";
 import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/Navbar";
 import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
